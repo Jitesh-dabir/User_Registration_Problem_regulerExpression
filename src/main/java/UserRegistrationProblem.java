27c40966b642b61bac13958ee2eb6fc9600ae686
Jitesh-Dabir <jitudabir9@gmail.com>
2020-03-05 17:03:13 +0530

    Added: Welcome to user registration problem

---
+++ src/main/java/UserRegistrationProblem.java
@@ -0,0 +1,9 @@
+import java.util.regex.Pattern;
+
+public class UserRegistrationProblem {
+
+    //WELCOME MESSAGE
+    public static void main(String[] args) {
+        System.out.println(" Welcome to User Registration Problem ");
+    }
+}
